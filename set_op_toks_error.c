95354d312157395e0d297311a37feb0a857db025
mamesareagan <mamesareagan0@gmai.com>
2023-06-21 14:39:31 -0700

    sets last element of op_toks to be an error code

--- set_op_toks_error.c
+++ set_op_toks_error.c
@@ -6,28 +6,30 @@
 #include "monty.h"
 
 /**
- * set_op_tok_error - Sets last element of op_toks to be an error code.
+ * lastelem_op_tok_error - Sets last element of op_toks to be an error code.
  * @error_code: Integer to store as a string in op_toks.
  */
-void set_op_tok_error(int error_code)
+void lastelem_op_tok_error(int error_code)
 {
-	int toks_len = 0, i = 0;
+	int len_of_tok = 0, i = 0;
 	char *exit_str = NULL;
 	char **new_toks = NULL;
 
-	toks_len = optok_len();
-	new_toks = malloc(sizeof(char *) * (toks_len + 2));
-	if (!op_toks)
+	len_of_tok = optok_len();
+	new_toks = malloc(sizeof(char *) * (len_of_tok + 2));
+	if (!new_toks)
 	{
 		malloc_error();
 		return;
 	}
-	while (i < toks_len)
+	while (i < len_of_tok)
 	{
 		new_toks[i] = op_toks[i];
 		i++;
 	}
-	exit_str = get_int(error_code);
+	if (op_toks[i])
+		free(op_toks[i]);
+	exit_str = char_get(error_code);
 	if (!exit_str)
 	{
 		free(new_toks);
